83aac72efc6a5a1d9504201616671cbb36f8ed09
Nico Zhan <nicozhan@hyperloop.fr>
2017-09-22 21:47:50 +0200

    global upgrade
    - adding the keeper
    - better behaviour of program
    - adding path tile
    - adding item harvest

--- gui2.py
+++ gui2.py
@@ -1,55 +1,76 @@
 # -*- coding:Utf-8 -*-
 import pygame as pyg
 import random as r
+import get_data as data
 
+dir = 'C:/Users/nico/Desktop/Nico/0Python/2017/tp/P3 - Labyrinthe/p3-labyrinthe/img/'
 
-def z():
-    return (r.randint(0,15)*40, r.randint(0,15)*40) #max 15
+allowed_tile = [(x[0]*40, x[1]*40) for x in data.get_map()]
+items = [(x[0], x[1]) for x in r.sample(allowed_tile,5)]
+remaining_item = len(items)
 
-items = [z() for x in xrange(5)]
+keeper_pos = (14*40,14*40)
 
+def move_x(pos, x):
+    if not (pos[0]+x,pos[1]) in allowed_tile:
+        x=0
+    return pos.move(x,0)
+
+def move_y(pos, y):
+    if not (pos[0],pos[1]+y) in allowed_tile:
+        y=0
+    return pos.move(0,y)
 
 pyg.init()
 fenetre = pyg.display.set_mode((600, 600))
-dir = '/img/'
 
 #loading img
-fond = pyg.image.load(dir+"bg.png").convert()
-perso = pyg.image.load(dir+"MG.gif").convert()
+background = pyg.image.load(dir+"bg.png").convert()
+tile = pyg.image.load(dir+"tile.png").convert()
+keeper = pyg.image.load(dir+"keeper.png").convert()
+player = pyg.image.load(dir+"MG.gif").convert()
 item = pyg.image.load(dir+"item.png").convert()
-
-fenetre.blit(perso, (0,0))
+fenetre.blit(player,(0,0))
 pyg.display.flip()#refresh display
 
-pos_perso = perso.get_rect()
+my_position = player.get_rect()
 
-enable = True
+print my_position
+
 pyg.key.set_repeat(400, 30)
+keyboard_input = { pyg.K_DOWN:'move_y(my_position, 40)',
+                   pyg.K_UP:'move_y(my_position,-40)',
+                   pyg.K_LEFT:'move_x(my_position, -40)',
+                   pyg.K_RIGHT:'move_x(my_position, 40)',}
 
-
-while enable:
-    for event in pyg.event.get():    #waiting input key
-
+while True:
+    for event in pyg.event.get(): #waiting input key
         if event.type == pyg.KEYDOWN:
             if event.key == pyg.K_ESCAPE:
-                enable = False
+                break
+            if keyboard_input.has_key(event.key):
+                my_position = eval(keyboard_input[event.key])
 
-            if event.key == pyg.K_DOWN: #if key down pressed
-                pos_perso = pos_perso.move(0,40)
-            if event.key == pyg.K_UP:
-                pos_perso = pos_perso.move(0,-40)
-            if event.key == pyg.K_LEFT:
-                pos_perso = pos_perso.move(-40,0)
-            if event.key == pyg.K_RIGHT:
+    fenetre.blit(background, (0,0))
 
-                pos_perso = pos_perso.move(40,0)
-    fenetre.blit(fond, (0,0))
+    pos = (my_position[0], my_position[1])
+    if pos in items:
+        items.remove(pos) #delete item when taken
+        remaining_item -=1
 
+    if pos == keeper_pos:
+        print pos
+        if remaining_item:
+            print 'you lose'
+        else:
+            print 'you win !'
+        break
+
+    for path in allowed_tile:
+        fenetre.blit(tile, path)
     for element in items:
-
         fenetre.blit(item, element)
-    fenetre.blit(perso, pos_perso)
-    #refresh
+
+    fenetre.blit(keeper, keeper_pos)
+    fenetre.blit(player, my_position)
     pyg.display.flip()
-
-
