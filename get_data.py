13b8419d79db16f78d9331c972922599b7d89190
Nico Zhan <nicozhan@hyperloop.fr>
2017-09-29 00:14:46 +0200

    pep8 upgrade

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 # -*- coding:Utf-8 -*-
 
+
 def get_map():
     data = ''
     x = y = 0
@@ -7,11 +8,12 @@ def get_map():
     with open('data', 'r') as f:
         data = f.read().split('\n')
 
-    for i, item in enumerate(data): #need reverse read
+    for i, item in enumerate(data):     # need reverse read
         y = i
         for h, letter in enumerate(item):
             if letter == 'O':
                 x = h
-                pos.append((x,y))
+                pos.append((x, y,))
 
     return pos
+
