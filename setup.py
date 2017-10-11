21ccfab04b57e71af706648fac6d6d01fa2b8500
hyperyon <nicozhan@hyperloop.fr>
2017-10-11 22:14:40 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+from setuptools import setup, find_packages
+import labyrinth
+ 
+with open('requirements.txt') as f:
+    requires = f.read().split('\n')
+
+
+
+setup(
+
+    name='oc-labyrinth',
+    version=3.2,
+ 
+    packages=find_packages(),
+    install_requires=requires,
+    author='Nico Zhan',
+    author_email='nicozhan@hyperloop.fr',
+    description='Help Mc Gyver to leave the maze',
+    long_description=open('README.md').read(),
+
+    # include file from manifest.in
+    include_package_data=True,
+    url='https://github.com/Hyperyon/p3-labyrinthe',
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Game',
+    ],
+ 
+)
